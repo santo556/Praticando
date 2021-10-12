9db5954d55260f5268ca04441cdba228d0cd51e9
santo556 <ve727402gafanhoto@gmail.com>
2021-10-12 09:51:39 -0400

    fazendo uma calculadora

---
+++ Teste/Calculadora/calc.js
@@ -0,0 +1,23 @@
+function insert(num) {
+    let numero = document.getElementById('res').innerHTML
+    document.getElementById('res').innerHTML = numero + num
+}
+
+function clean() {
+    document.getElementById('res').innerHTML = ""
+}
+
+function back() {
+    let resultado = document.getElementById('res').innerHTML
+    document.getElementById('res').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() {
+    let resultado = document.getElementById('res').innerHTML
+    if(resultado) {
+        document.getElementById('res').innerHTML = eval(resultado)
+    }
+    else{
+        document.getElementById('res').innerHTML = "Nada..."
+    }
+}
